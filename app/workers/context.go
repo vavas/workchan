d4dba8f64fde590c3de0b54ef68734e92ef2690b
vavas <volodymyr.kryzhanovskiy@deliveroo.co.uk>
2021-05-10 11:31:37 +0300

    created kafka publisher worker

--- app/workers/context.go
+++ app/workers/context.go
@@ -3,9 +3,19 @@ package workers
 import (
 	"crypto/md5"
 	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gocraft/work"
+	"github.com/sirupsen/logrus"
+	"github.com/vavas/workchan/pkg/db"
 	"time"
 )
+
+type Context struct {
+	Logger        logrus.Logger
+	DB            *db.DB
+	KafkaProducer *kafka.Producer
+	JobID         string
+}
 
 func uniqueJobID(job *work.Job) string {
 	uniqueStr := fmt.Sprintf("%v %v %v", job.Name, job.Args, time.Now())
---
+++ app/workers/kafka_publisher.go
@@ -0,0 +1,11 @@
+package workers
+
+import (
+	"github.com/gocraft/work"
+)
+
+func (c *Context) KafkaPublishEventJob(job *work.Job) error {
+	//ctx := context.Background()
+
+	return nil
+}
